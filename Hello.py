1ce87240335a40e8bb49dd64b5a3da5245a5aea6
karthik <karthiksarma456@gmail.com>
2024-12-03 11:33:56 +0530

    first programming to add

---
+++ Hello.py
@@ -0,0 +1,8 @@
+def fun():
+    print (" Hello! want to know about the function..")
+    a =int(input("type the firstValue: "))
+    b= int(input("type the secondValue: "))
+    print("The addition values is: ",(a+b))
+
+fun()
+
